9c95e3c53feadc5930b3c7b419555bdcd36b3393
Mathias323 <420mathias420@gmail.com>
2026-03-24 16:49:37 +0100

    Forgot to press save

--- Starter_FEC_1.py
+++ Starter_FEC_1.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-##this is just the initial build, for educational purposes, i used the notes from mit page 10 and 11 as the theory to do it
-##i dont even think it qualifies as a bch code, but it does follow a lot of the basic principles.
-##it is not optimal by any means and is bare minimum.
-##it is a 1 error correcting cyclical non-systematic code.
+#Initial build of a block code using a primitive polynomial, it was build mainly to understand the fundamentals
+#it has 2 different encoding/decoding versions, 1 that is based on simple algebra.
+#and the second version that is systematic, meaning that the message is visible as plaintext in the codeword
+#this is an improvement both for slightly more simply decoding, and especially for bugfixing and clarity.
 
 class BCH_basic:
     def __init__(self, polynomial, error_correcting_amount):
@@ -42,6 +42,7 @@ class BCH_basic:
 
     def divide_polynomial(self, polynomial_a,polynomial_divider): 
         #finds the remainder by xoring the generator polynomium alinged with the current bit index
+        #returns the quotient as index 0 and returns remainder at index 1
         #starting at msb, returns array length div degree -1 since the remainder will always be smaller than the divisor
         #i do not know how this works, consult the notes if you want to know the theory.
         a_deg=len(polynomial_a)-1
@@ -62,8 +63,31 @@ class BCH_basic:
             return primitive
         else:
             pass
+        
 
+    def encode_systematic(self, message): # takes message and shifts it by appending 0's, then calculates what those 0's should be in order for the remainder to be 0
+        if len(message) != self.k:
+            raise ValueError(f"Message must have length {self.k}")
+        if not isinstance(message, np.ndarray):
+            raise ValueError("Message must be a numpy array")
+        
+        padding=np.zeros(len(self.g)-1,dtype=int)
+        message_padded=np.append(padding,message)
+        remainder=self.divide_polynomial(message_padded,self.g)[1]
+        message_padded[0:len(remainder)]=remainder
 
+        return message_padded
+
+    def decode_systematic(self, codeword):  #decode via a simple algebraic rule, and can corrects 1 error via to the linearity of syndrome function
+        message, syndrome=self.divide_polynomial(codeword,self.g)
+        if np.all(syndrome == 0):
+            return codeword[self.n-self.k:self.n+1]
+        for i in range(len(self.syndrome_box)):
+            if np.array_equal(syndrome,self.syndrome_box[i]):
+                codeword[i]^=1
+                return codeword[self.n-self.k:self.n+1]
+        return "message for sure is errored" #doesnt really happen if you try to error correct.
+    
     def encode(self, message): # takes message and multiplies it by the generator, there is no reduction, since we know it wont go out of bounds
         if len(message) != self.k:
             raise ValueError(f"Message must have length {self.k}")
@@ -99,12 +123,17 @@ encoder=BCH_basic(primitive_polynomial,1)
 #print("n =", encoder.n)
 #print("g =", encoder.g)
 #print("k =", encoder.k)
+
 mess=encoder.random_message(11)
-encoded_mess=encoder.encode(mess)
+encoded_mess=encoder.encode_systematic(mess)
 
 print(mess)
 print(encoded_mess)
-encoded_mess[0]^=1
+encoded_mess[2]^=1
 decoded=encoder.decode(encoded_mess)
-print(decoded,mess,np.array_equal(decoded,mess))
+print(decoded)
+print(np.array_equal(mess,decoded))
+#print(decoded,mess,np.array_equal(decoded,mess))
 
+
+ 
